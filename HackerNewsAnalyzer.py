9cbaac5ccb986ada85002ec141c70f5e0fb97901
Morsal Niyaz <morsalniyaz@gmail.com>
2019-01-26 11:24:04 -0800

    hackernewscode

--- HackerNewsAnalyzer.py
+++ HackerNewsAnalyzer.py
@@ -2,41 +2,6 @@
 
 import requests
 
-response = requests.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
-
-# Get top 5 ids
-top_five_ids = response.json()[:5]
-
-# Fetch top 5 stories
-
-stories = []
-
-for top_id in top_five_ids:
-    curr_story_api_url = 'https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'.format(id)
-    curr_story = requests.get(curr_story_api_url)
-    stories.append(curr_story.json())
-
-listOfStories = []
-
-for story in stories:
-    title = story['title'] #gets the title of the current story
-    comments = [] #creates an empty comments list every iteration of the loop
-    dictionary = {}  #creates a new dictionary every iteration of the loop
-   #print("\nTitle: " + title + "\n")
-    for kid in story['kids'][:5]:
-       #print(kid)
-       kid_url = requests.get('https://hacker-news.firebaseio.com/v0/item/' +str(kid)+'.json?print=pretty')
-       #print(kid_url.json()['text'])
-       comment = kid_url.json()['text'] #puts the current comment text in a variable
-       #print(comment)
-       comments.append(comment) #adds the comment to the current comment list
-    dictionary[title] = comments #sets key-value pair of title and list of comments
-    listOfStories.append(dictionary) #add the current dictionary to the listOFStories
-
-#print results
-#for l in listOfStories:
-    #print(l)
-    #print("\n")
 
 class HackerNews:
     with open('/Users/MorsalNiyaz/Development/polyglot/cohort-1/data/positive_words.txt', 'r') as open_pos:
@@ -71,15 +36,58 @@ class HackerNews:
             return "inconclusive" 
 
         for sentence in listOfStories:
-            hacker = sentence.split(",")[3].strip()
+            hacker = sentence.split(",").strip()
 
             analyze_news(hacker)
     
-    hacker = input("What do you want to analyze?")
-    analyze_news(hacker)
-    print(analyze_news(hacker))
+ #   hacker = input("What do you want to analyze?")
+  #  analyze_news(hacker)
+  #  print(analyze_news(hacker))
+#
 
 
+response = requests.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
+
+# Get top 5 ids
+top_five_ids = response.json()[:5]
+print(top_five_ids)
+
+
+
+# Fetch top 5 stories
+
+stories = []
+
+for top_id in top_five_ids:
+    curr_story_api_url = 'https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'.format(top_id)
+    curr_story = requests.get(curr_story_api_url)
+    print(curr_story.json())
+    stories.append(curr_story.json())
+
+#print(stories)
+
+listOfStories = []
+
+for story in stories:
+    print(story)
+    title = story['title'] #gets the title of the current story
+    comments = [] #creates an empty comments list every iteration of the loop
+    title_comments_d = {}  #creates a new dictionary every iteration of the loop
+    print("\nTitle: " + title + "\n")
+    for kid in story['kids'][:5]:
+        print(kid)
+        kid_url = requests.get('https://hacker-news.firebaseio.com/v0/item/' +str(kid)+'.json?print=pretty')
+        print(kid_url.json()['text'])
+        comment = kid_url.json()['text'] #puts the current comment text in a variable
+        print(comment)
+        comments.append(comment) #adds the comment to the current comment list
+        title_comments_d[title] = comments #sets key-value pair of title and list of comments
+    listOfStories.append(title_comments_d) #add the current dictionary to the listOFStories
+
+#print results
+for l in listOfStories:
+    print(l)
+    print("\n")
 
 
    
